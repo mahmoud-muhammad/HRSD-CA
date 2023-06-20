475983038b62334b7a8b20d077ecbbbfd63c3664
Hossam Eraqy <Hossam.Eraqy@gmail.com>
2023-06-20 11:34:07 +0300

    Upload HTML Files

---
+++ HTML/js/utilities.js
@@ -0,0 +1,50 @@
+function detectBrowserVersion() {
+    var ua = navigator.userAgent,
+        tem,
+        M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+    if (/trident/i.test(M[1])) {
+        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+
+        return 'IE' + (tem[1] || '');
+    }
+    if (M[1] === 'Chrome') {
+        tem = ua.match(/\bOPR\/(\d+)/)
+        if (tem != null) return 'Opera ' + tem[1];
+    }
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+
+    return M.join('');
+};
+
+$(function() {
+
+    //Detect your browser version and set class name in html tag
+    var _detectBrowser = detectBrowserVersion();
+    $("html").addClass(_detectBrowser);
+
+    $('.progress-bar').css("width", function() {
+        return $(this).attr("aria-valuenow") + "%";
+    })
+
+    var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 0, // distance to the element when triggering the animation (default is 0)
+        mobile: true, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    });
+    wow.init();
+
+});
+
+
+$(window).on("load", function() {
+    
+});
